e839ab4f997d66b4017d0c0ccdba9438141aa749
Leung <leungwn@outlook.com>
2020-07-27 15:43:35 +0800

    objc4-781 add debug target

---
+++ objc4-781/objc4_debug/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  objc4_debug
+//
+//  Created by Leung on 27/07/2020.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
