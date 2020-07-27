323bc03c968721f5e226fe96f2235b10670ba50c
Leungwn <2042805653@qq.com>
2020-07-27 16:30:04 +0800

    modify test

--- objc4-781/objc4_debug/main.m
+++ objc4-781/objc4_debug/main.m
@@ -7,10 +7,17 @@
 
 #import <Foundation/Foundation.h>
 
+#import <objc/runtime.h>
+#import <objc/message.h>
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Hello, World!");
+        Class newClass = objc_allocateClassPair(objc_getClass("NSObject"), "newClass", 0);
+                objc_registerClassPair(newClass);
+        id newObject = [[newClass alloc]init];
+        NSLog(@"%@",newObject);
+        NSLog(@"NO PAY NO GAIN");
     }
     return 0;
 }
